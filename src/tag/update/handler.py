3979ee999ee538de47e631e12a13b9592bc9c911
gabmissil@gmail.com <gabriel.missio@CTNDELL6QJCP63.pampa.compasso>
2021-01-28 16:06:20 -0300

    feat(tag): created put funtion to tag

--- src/tag/update/handler.py
+++ src/tag/update/handler.py
@@ -1,7 +1,7 @@
 import sys
 sys.path.append('/opt')
 import json
-import service.spent_service as spent_service
+import service.tag_service as tag_service
 from utils.http_response import create_success_body, create_error_body
 from utils.validation import spent_validation as spent_validation
 
@@ -9,11 +9,11 @@ def handler(event, context):
     try:#to-do: to implament filters and pagination
         id = event['pathParameters']['id']
         payload = json.loads(event['body'])
-        #print(payload)
+        print(payload)
         #spent_validation.validate(payload)
-        #result = spent_service.update_spent_by_id(id, payload)
-        #print(result)
-        return create_success_body('endpoint update tag')
+        result = tag_service.update_tag_by_id(id, payload)
+        print(result)
+        return create_success_body(result)
     except Exception as err:
         print(err)
         return create_error_body(err)
